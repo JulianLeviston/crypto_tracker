9d1a479ee523c9e43a5d1ef9b6b25f22f7954df2
Julian Leviston <julian@leviston.net>
2025-08-06 18:26:09 +1000

    doc: add description for currency exchange type model

--- app/models/currency_exchange_type.rb
+++ app/models/currency_exchange_type.rb
@@ -1,3 +1,4 @@
+# CurrencyExchangeType is the type of an ExchangeRate eg BTCAUD or ETHAUD including its source url
 class CurrencyExchangeType < ApplicationRecord
   belongs_to :from_currency, class_name: "Currency"
   belongs_to :to_currency, class_name: "Currency"
