f5327833691bf31d9834ff861429b9eefc6d4c66
Julian Leviston <julian@leviston.net>
2025-08-06 17:26:29 +1000

    feat: add currency model and seed

---
+++ app/models/currency.rb
@@ -0,0 +1,3 @@
+# Currency is one side of a CurrencyExchangeType eg AUD, BTC or ETH
+class Currency < ApplicationRecord
+end
---
+++ db/migrate/20250806070857_create_currencies.rb
@@ -0,0 +1,10 @@
+class CreateCurrencies < ActiveRecord::Migration[8.0]
+  def change
+    create_table :currencies do |t|
+      t.string :code
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
